dcde38a430c532177442d86d85c8bad42c56c755
christianzaboli <zabolichristian@gmail.com>
2025-11-17 11:35:17 +0100

    FE inizio setup frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,30 @@
+import "./App.css";
+// DIPENDENZE REACT
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css'
+// LAYOUT
+import DefaultLayout from "./Layout/DefaultLayout";
+
+// CONTEXTS
+import { DefaultProvider } from "./Contexts/DefaultContext";
+
+// PAGINE
+import HomePage from "./Pages/HomePage";
 
 function App() {
-
   return (
     <>
-
+      <DefaultProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route index element={<HomePage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </DefaultProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Space Domiciles</h1>
+    </div>
+  );
+}
