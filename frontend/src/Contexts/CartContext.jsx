4158a3f4e9094031446d452760fdd1827359a045
christianzaboli <zabolichristian@gmail.com>
2025-11-24 19:49:32 +0100

    fix ale

--- frontend/src/Contexts/CartContext.jsx
+++ frontend/src/Contexts/CartContext.jsx
@@ -53,6 +53,7 @@ export function CartProvider({ children }) {
 
       if (newQty <= 0) {
         delete copy[id];
+        return copy;
       }
       if (newQty > maxStock) {
         setToastMessage("Hai raggiunto la massima quantitá disponibile!");
